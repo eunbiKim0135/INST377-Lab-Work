f3be0e2a3f43583d5f656ac8e0b94e1533ca3e3a
Eunbi Kim <ekim0135@terpmail.umd.edu>
2022-11-04 22:28:06 -0400

    updated doodle

---
+++ client/Doodle/app.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid');
+  const doodler = document.createElement('div');
+  const doodlerLeftSpace = 50;
+  const doodlerBottomSpace = 150;
+  const isGameOver = false;
+  const platformCount = 5;
+  let platforms = []
+
+  function createDoodler() {
+    grid.appendChild(doodler);
+    doodler.classList.add('doodler');
+    doodler.style.left = `${doodlerLeftSpace}px`;
+    doodler.style.bottom = `${doodlerBottomSpace}px`;
+  }
+
+  class Platform {
+    constructor(newPlatBottom) {
+      this.bottom = newPlatBottom;
+      this.left = Math.random() * 315;
+      this.visual = document.createElement('div');
+
+      const {visual} = this;
+      visual.classList.add('platform');
+      visual.style.left = `${this.left}px`;
+      visual.style.bottom = `${this.bottom}px`;
+      grid.appendChild(visual);
+    }
+  }
+
+  function createPlatform() {
+    for (let i = 0; i < platformCount; i++) {
+      const platGap = 600 / platformCount;
+      const newPlatBottom = 100 + i * platGap;
+      const newPlatform = new Platform(newPlatBottom);
+      platforms.push(newPlatform)
+      console.log(platforms)
+    }
+  }
+
+  function start() {
+    if (!isGameOver) {
+      createDoodler();
+      createPlatform();
+    }
+  }
+  // attach to button
+  start();
+});
